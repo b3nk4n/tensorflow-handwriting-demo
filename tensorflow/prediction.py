94b62fdbd3248310db5142dc2d77286666aed8dc
bsautermeister <apps@bsautermeister.de>
2017-05-01 21:51:34 +0200

    Changed scaling

--- tensorflow/prediction.py
+++ tensorflow/prediction.py
@@ -1,5 +1,5 @@
 import utils.ui
 
-dialog = utils.ui.CanvasDialog("Read Handwriting...", 320, 320)
+dialog = utils.ui.CanvasDialog("Read Handwriting...", 32, 32)
 data = dialog.show()
 print(data)
--- tensorflow/utils/ui.py
+++ tensorflow/utils/ui.py
@@ -3,19 +3,20 @@ from Tkinter import *
 
 class CanvasDialog:
     
-    def __init__(self, title, width, height, thickness=20, resultScale=10):
+    def __init__(self, title, width, height, thickness=20, scale=10):
         self.title = title
         self.width = width
         self.height = height
         self.thickness = thickness
-        self.resultScale = resultScale
+        self.scale = scale
         
         self.root = Tk()
         self.root.title(title)
         self.root.resizable(0,0)
         self.root.protocol("WM_DELETE_WINDOW", self._closing)
 
-        self.canvas = Canvas(self.root, bg="white", width=width, height=height)
+        self.canvas = Canvas(self.root, bg="white",
+                             width=width*scale, height=height*scale)
         self.canvas.configure(cursor="crosshair")
         self.canvas.pack()
         self.canvas.bind("<Button-1>", self._mouse_left)
@@ -46,15 +47,16 @@ class CanvasDialog:
     def _get_data(self):
         counter = 0
         scaledImg = []
-        for y in range(0, self.height, self.resultScale):
-            for x in range(0, self.width, self.resultScale):
-                scaledImg.append(self._get_pixel_group(x, y,
-                                                       self.resultScale,
-                                                       self.resultScale));
+        for y in range(self.height):
+            for x in range(self.width):
+                scaledImg.append(self._get_pixel_group(x*self.scale,
+                                                       y*self.scale,
+                                                       self.scale,
+                                                       self.scale));
         return scaledImg
 
     def _mouse_left(self, event):
-        print(self.get_pixel(event.x, event.y))
+        print(self._get_pixel(event.x, event.y))
         self.canvas.create_oval(event.x - self.thickness / 2,
                                 event.y - self.thickness / 2,
                                 event.x + self.thickness / 2,
