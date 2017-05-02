686f313e82a28552b180f813f882f7c169acf3c2
bsautermeister <apps@bsautermeister.de>
2017-05-02 09:04:47 +0200

    Refactored train params utils function

--- tensorflow/training.py
+++ tensorflow/training.py
@@ -9,17 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-
-def get_num_trainable_params():
-    total_parameters = 0
-    for variable in tf.trainable_variables():
-        # shape is an array of tf.Dimension
-        shape = variable.get_shape()
-        variable_parametes = 1
-        for dim in shape:
-            variable_parametes *= dim.value
-        total_parameters += variable_parametes
-    return total_parameters
+import utils.tensor
 
 
 def main(_):
@@ -132,7 +122,7 @@ def main(_):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         
-        print('\nModel with {} trainable parameters.'.format(get_num_trainable_params()))
+        print('\nModel with {} trainable parameters.'.format(utils.tensor.get_num_trainable_params()))
         time.sleep(3)
         print('\nTraining...')
 
