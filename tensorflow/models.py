e9c1da31886ffa1c2c155cfe3e82ddd80be8b5cd
bsautermeister <apps@bsautermeister.de>
2017-05-03 23:40:38 +0200

    Fixed bug in fully connected method

--- tensorflow/models.py
+++ tensorflow/models.py
@@ -2,42 +2,26 @@ import numpy as np
 import tensorflow as tf
 
 
-def neural_net(x, keep_prob, weight_decay):
-    y = tf.contrib.layers.fully_connected(x, 32,
-                                            weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                            weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
-                                            activation_fn=tf.nn.relu)
-    y = tf.contrib.layers.fully_connected(y, 32,
-                                            weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                            weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
-                                            activation_fn=tf.nn.relu)
+def neural_net(x, layers, keep_prob, weight_decay):
+    y = x
+    for layer in layers[:-1]:
+        y = tf.contrib.layers.fully_connected(x, layer,
+                                              weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                              weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
+                                              activation_fn=tf.nn.relu)
     y = tf.nn.dropout(y, keep_prob=keep_prob)
-    return tf.contrib.layers.fully_connected(y , 26,
-                                                weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                                weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay))
+    return tf.contrib.layers.fully_connected(y , layers[-1],
+                                             weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                             weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay))
 
-def conv_net(x, keep_prob):
+def conv_net(x, convs, fullys, keep_prob, weight_decay):
     y = tf.reshape(x, [-1, 32, 32, 1])
-
-    y = tf.contrib.layers.conv2d(y, 8, kernel_size=[5, 5], stride=[1, 1], padding='SAME',
-                                    weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                    weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
-                                    activation_fn=tf.nn.relu)
-    y = tf.contrib.layers.max_pool2d(y, kernel_size=[2, 2], padding='SAME')
-    y = tf.contrib.layers.conv2d(y, 8, kernel_size=[3, 3], stride=[1, 1], padding='SAME',
-                                    weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                    weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
-                                    activation_fn=tf.nn.relu)
-    y = tf.contrib.layers.max_pool2d(y, kernel_size=[2, 2], padding='SAME')
+    for conv in convs:
+        y = tf.contrib.layers.conv2d(y, conv[0], kernel_size=[conv[1], conv[1]], stride=[1, 1], padding='SAME',
+                                        weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                        weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
+                                        activation_fn=tf.nn.relu)
+        y = tf.contrib.layers.max_pool2d(y, kernel_size=[2, 2], padding='SAME')
     
     y = tf.reshape(y, [-1, np.prod(y.get_shape().as_list()[1:])])
-
-    y = tf.contrib.layers.fully_connected(y, 32,
-                                            weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                            weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay),
-                                            activation_fn=tf.nn.relu)
-    
-    y = tf.nn.dropout(y, keep_prob=keep_prob)
-    return tf.contrib.layers.fully_connected(y, 26,
-                                                weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                                weights_regularizer=tf.contrib.layers.l2_regularizer(weight_decay))
+    return neural_net(y, fullys, keep_prob, weight_decay)
